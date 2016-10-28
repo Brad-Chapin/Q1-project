7bf672a047e1693505fa28ec228bda25131bbaf0
Brad Chapin <paledoor@yahoo.com>
2016-10-28 12:52:45 -0600

    API connected, waiting on CORS fix

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 $( document ).ready(function() {
-
+  var userText = "placeholder text";
+  var $xhr = $.getJSON("http://isithackday.com/arrpi.php?text=userText?&format=json");
+  $xhr.done (function(data){
+    if ($xhr.status !== 200){
+      return;
+    } else {
+      console.log(data);
+    }
+  });
 });
+
+
+//http://isithackday.com/arrpi.php?text="variable"?&format=json
+// where variable is set in the code and it's value is determined dynamically
