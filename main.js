4eaead8bcd8052663ca77b72efa5023c41850a43
Brad Chapin <paledoor@yahoo.com>
2016-10-31 16:49:16 -0600

    continued presentation improvements, working on dice logic

--- main.js
+++ main.js
@@ -16,24 +16,25 @@ $(document).ready(function() {
 
     });
     $("#start").on("click", function() {
-      console.log("test");
       //game mode validation
-        if ($("test1").prop("checked") == false && $("test2").prop("checked") == false) {
+        if ($("#test1").prop("checked") == false && $("#test2").prop("checked") == false) {
             alert("Ye have t' choose a game mode, ye howlin' nutter.")
-        } else if ($("test1").prop("checked")) {
+        } else if ($("#test1").prop("checked")) {
           playerOne = prompt("Tell us yer name, so we know what t'call ye.")
-        } else if ($("test2").prop("checked")) {
+        } else if ($("#test2").prop("checked")) {
           playerOne = prompt("Tell us th' first mate's name")
           playerTwo = prompt("Tell us th' gunner's name.")
         };
     });
     $("#toss").on("click", function(){
       var diceBoxes = ["#one", "#two", "#three", "#four", "#five"];
-      for (var i = 0; i < diceBoxes.length; i++){
-        $(diceBoxes[i]).text((Math.floor(Math.random()*6)+1));
+      var rolls = [];
       }
-
-    });
+      for (i = 0; i < diceBoxes.length; i++){
+        rolls.push((Math.floor(Math.random()*6)+1));
+        console.log(rolls);
+      }
+  });
 });
 var turn = "";
 var round = 1;
