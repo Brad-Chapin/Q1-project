331a66c8bc115be0e6e7b8a846001688f74825c0
Brad Chapin <paledoor@yahoo.com>
2016-11-01 13:54:28 -0600

    654 sorting logic revamped, function structure outlined

--- main.js
+++ main.js
@@ -26,22 +26,50 @@ $(document).ready(function() {
           playerTwo = prompt("Tell us th' gunner's name.")
         };
     });
-    $("#toss").on("click", function(){
-      var diceBoxes = ["#one", "#two", "#three", "#four", "#five"];
-      var rolls = [];
-      }
-      for (i = 0; i < diceBoxes.length; i++){
-        rolls.push((Math.floor(Math.random()*6)+1));
-        console.log(rolls);
-      }
+        $("#toss").on("click", turn);
   });
-});
+
+//variables
 var turn = "";
-var round = 1;
 var playerOne = "";
 var playerTwo = "";
 var playerOneScore = 0;
 var playerTwoScore = 0;
+var dice = 5;
+var round = 0;
+
+function turn (){
+  round++;
+  storedRolls = round ([]);
+};
+
+function round (storedRolls){
+  var rolls = [];
+    var diceBoxes = ["#one", "#two", "#three", "#four", "#five"];
+    for (var d = 0; d < dice; d++){
+      rolls.push((Math.floor(Math.random()*6)+1));
+      $(diceBoxes[d]).text(rolls[d]);
+    }
+    for (var s = 0; s < dice; s++){
+      if (rolls.indexOf(6) !== -1 && storedRolls.length == 0){
+        storedRolls.push(diceBoxes[rolls.indexOf(6)]);
+        dice--;
+      } else if (rolls.indexOf(5) !== -1 && storedRolls.length == 1){
+        storedRolls.push(diceBoxes[rolls.indexOf(5)]);
+        dice--;
+      } else if (rolls.indexOf(4) !== -1 && storedRolls.length == 2){
+        storedRolls.push(diceBoxes[rolls.indexOf(4)]);
+        dice--;
+      }
+    }
+};
+
+function reset (){
+  //resets the values for the next turn
+  dice = 5;
+  round = 0;
+}
+
 
 //"https://g-hackday.herokuapp.com/arrpi.php?text=" + userText + "&format=json"
 // where variable is set in the code and it's value is determined dynamically
